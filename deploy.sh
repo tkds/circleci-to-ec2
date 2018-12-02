9758499049b2937c0b207637f25783d619f1821c
tkds <38493520+tkds@users.noreply.github.com>
2018-12-02 19:40:41 +0900

    first commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ex
+
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION="us-east-1"
+
+MYSECURITYGROUP=${AWS_SECURITY_GROUP}
+EC2_HOST=${AWS_EC2_HOST}
+MYIP=`curl -s ifconfig.me`
+
+aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
+ssh -o "StrictHostKeyChecking=no" ec2-user@$EC2_HOST sh <<SHELL
+touch test.txt
+SHELL
+aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
